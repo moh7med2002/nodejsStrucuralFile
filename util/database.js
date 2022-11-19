c3acfb96fe85101d9f49ee683d5dde9c0c8c4f78
moh7amed2002 <moha7medheshamabohani@gmail.com>
2022-11-19 17:35:49 +0200

    new database

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize')
 
 // const sequelize = new Sequelize('school_db','root','059283805928388',{dialect:'mysql',host:"localhost"});
 
-const sequelize = new Sequelize('freedb_school_db','freedb_school_user','Y?Y%GRaW4XFthp9',{dialect:'mysql',host:"sql.freedb.tech"});
+const sequelize = new Sequelize('freedb_myschool','freedb_schooluser','TPf8*Ttzkn8mPKF',{dialect:'mysql',host:"sql.freedb.tech"});
 
 
 module.exports = sequelize;
