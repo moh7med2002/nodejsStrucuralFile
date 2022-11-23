3feff33db5d05d6923f9434099fbc63c441d660a
moh7amed2002 <moha7medheshamabohani@gmail.com>
2022-11-23 19:59:47 +0200

    database

--- util/database.js
+++ util/database.js
@@ -1,8 +1,11 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('rescteeh_schools_db','rescteeh_root_user','059283805928388',{dialect:'mysql',host:"localhost"})
+// const sequelize = new Sequelize('rescteeh_schools_db','rescteeh_root_user','059283805928388',{dialect:'mysql',host:"localhost"})
 
 // const sequelize = new Sequelize('school_db','root','059283805928388',{dialect:'mysql',host:"localhost"});
+
+
+const sequelize = new Sequelize('academ58_schools_db','academ58_root','059283805928388',{dialect:'mysql',host:"localhost"})
 
 
 
