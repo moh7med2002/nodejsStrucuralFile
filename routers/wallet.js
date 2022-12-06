076443d1b7c03f87a1e16acd9690328d4c548d8d
moh7amed2002 <moha7medheshamabohani@gmail.com>
2022-12-06 16:16:36 +0200

    add dynamic router to accept and reject

--- routers/wallet.js
+++ routers/wallet.js
@@ -7,9 +7,9 @@ const walletControllers = require('../controllers/wallet');
 
 router.post('/student/add' , studentAuth , walletControllers.addMoneyForWallet);
 
-router.post('/admin/accept/walletId' , adminAuth ,  walletControllers.acceptMoney);
+router.post('/admin/accept/:walletId' , adminAuth ,  walletControllers.acceptMoney);
 
-router.post('/admin/reject/walletId' , adminAuth , walletControllers.rejectMoney);
+router.post('/admin/reject/:walletId' , adminAuth , walletControllers.rejectMoney);
 
 router.get('/admin/wallets/all' , adminAuth , walletControllers.getWallets);
 
