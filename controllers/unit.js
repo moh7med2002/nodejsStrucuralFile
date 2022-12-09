910786c08872e224a304e4d9e18519a6bec60f1b
moh7amed2002 <moha7medheshamabohani@gmail.com>
2022-12-09 12:35:37 +0200

    teacher unit function

--- controllers/unit.js
+++ controllers/unit.js
@@ -1,7 +1,24 @@
 const Unit = require('../models/Unit');
 
 
-exports.createUnit = async(req,res,next)=>
+exports.createUnitByAdmin = async(req,res,next)=>
+{
+    try{
+        const {title,CourseId} = req.body
+        const unit = new Unit(req.body)
+        await unit.save()
+        res.status(201).json('تم انشاء الوحدة')
+    }
+    catch(err){
+        if(! err.statusCode){
+            err.statusCode=500;
+        }
+        next(err);
+    }
+}
+
+
+exports.createUnitByTeacher = async(req,res,next)=>
 {
     try{
         const {title,CourseId} = req.body
@@ -68,7 +85,7 @@ module.exports.getUnitExams = async (req,res,next) => {
     }
 }
 
-module.exports.updateUnit = async (req,res , next)=>{
+module.exports.updateUnitByAdmin = async (req,res , next)=>{
     const {unitId} = req.params;
     const {title} = req.body;
     try{
@@ -92,7 +109,52 @@ module.exports.updateUnit = async (req,res , next)=>{
 }
 
 
-module.exports.deleteUnit = async (req,res,next)=>{
+module.exports.updateUnitByTeacher = async (req,res , next)=>{
+    const {unitId} = req.params;
+    const {title} = req.body;
+    try{
+        const unit = await Unit.findOne({where:{id:unitId}});
+        if(!unit)
+        {
+            const error = new Error('الوحدة غير موجود')
+            error.statusCode = 404
+            throw error;
+        }
+        unit.title = title;
+        await unit.save();
+        res.status(201).json({message:"تم تحديث الوحدة بنجاح"});
+    }
+    catch(err){
+        if(! err.statusCode){
+            err.statusCode=500;
+        }
+        next(err);
+    }
+}
+
+
+module.exports.deleteUnitByAdmin = async (req,res,next)=>{
+    const {unitId} = req.params;
+    try{
+        const unit = await Unit.findOne({where:{id:unitId}});
+        if(!unit)
+        {
+            const error = new Error('الوحدة غير موجود')
+            error.statusCode = 404
+            throw error;
+        }
+        await unit.destroy();
+        res.status(201).json({message:"تم حذف الوحدة بنجاح"});
+    }
+    catch(err){
+        if(! err.statusCode){
+            err.statusCode=500;
+        }
+        next(err);
+    }
+}
+
+module.exports.deleteUnitByteacher = async (req,res,next)=>{
     const {unitId} = req.params;
     try{
         const unit = await Unit.findOne({where:{id:unitId}});
--- routers/unit.js
+++ routers/unit.js
@@ -3,13 +3,27 @@ const router = express.Router();
 const unitController = require('../controllers/unit');
 
 const adminAuth = require('../middelware/AdminAuth');
+const teacherAuth = require('../middelware/TeacherAuth');
 
-router.post('/create', adminAuth ,unitController.createUnit);
-router.put('/:unitId' , adminAuth , unitController.updateUnit);
+router.post('/create', adminAuth ,unitController.createUnitByAdmin);
+router.post('/teacher/create', teacherAuth ,unitController.createUnitByTeacher);
+
+
+
+router.put('teacher/:unitId' , teacherAuth , unitController.updateUnitByTeacher);
+router.put('/:unitId' , adminAuth , unitController.updateUnitByAdmin);
+
+
+
 router.get('/exams/:unitId' , unitController.getUnitExams);
+
 router.get('/course/:courseId' , unitController.getCourseUnit);
+
 router.get('/all' , unitController.getUnits);
-router.delete('/:unitId' , adminAuth ,unitController.deleteUnit);
+
+
+router.delete('/teacher/:unitId' , teacherAuth ,unitController.deleteUnitByteacher);
+router.delete('/:unitId' , adminAuth ,unitController.deleteUnitByAdmin);
 
 
 module.exports = router;
