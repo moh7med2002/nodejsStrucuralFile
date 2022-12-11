e3338a64d523279bb568a0e7ee01e935a94e1046
moh7amed2002 <moha7medheshamabohani@gmail.com>
2022-12-11 16:03:46 +0200

    create exam by teacher

--- routers/exam.js
+++ routers/exam.js
@@ -6,12 +6,25 @@ const studentAuth = require('../middelware/StudentAuth');
 
 const adminAuth = require('../middelware/AdminAuth');
 
+const teacherAuth = require('../middelware/TeacherAuth');
 
-router.post('/create' , adminAuth , examController.createExam)
+
+router.post('/teacher/create' , teacherAuth , examController.createExam );
+router.post('/create' , adminAuth , examController.createExam);
+
+
+router.post('/teacher/question/create/:examId' , teacherAuth ,examController.createQuestion);
 router.post('/question/create/:examId' , adminAuth ,examController.createQuestion);
+
+
 router.post('/mark/:examId' , studentAuth ,examController.markExam);
+
 router.get('/student/:ExamId' , studentAuth ,examController.getExamForStudent);
+
 router.get('/:ExamId',examController.getExam);
+
+
+router.put('/teacher/:examId' , teacherAuth ,examController.updateExam);
 router.put('/:examId' , adminAuth ,examController.updateExam);
 
 module.exports = router;
