76f1c39605a6cf75320925a63f9d2bf6dcadfa4e
shathakh <shathaqudaih17@gmail.com>
2023-01-09 20:14:29 +0200

    build database

--- util/database.js
+++ util/database.js
@@ -1,14 +1,16 @@
 const Sequelize = require('sequelize')
 
+const DEV_DATABASE_URL='postgres://school_shatha:123456@localhost:5432/school_db'
 
 
 
 // local db
 // const sequelize = new Sequelize('school_db','root','059283805928388',{dialect:'mysql',host:"localhost"});
+const sequelize = new Sequelize(DEV_DATABASE_URL,{dialect:'postgres',host:"localhost"});
 
 
 //  academic db
-const sequelize = new Sequelize('academ58_schools_db','academ58_root','059283805928388',{dialect:'mysql',host:"localhost"});
+// const sequelize = new Sequelize('academ58_schools_db','academ58_root','059283805928388',{dialect:'mysql',host:"localhost"});
 
 
 
