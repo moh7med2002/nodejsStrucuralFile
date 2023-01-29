575089180fb2ba535fe2b5050ebdd456870fb418
moh7amed2002 <moha7medheshamabohani@gmail.com>
2023-01-29 12:08:18 +0200

    edit subjectId to SubjectId

--- controllers/Forum.js
+++ controllers/Forum.js
@@ -5,7 +5,7 @@ exports.createForum = async (req, res, next) => {
     const { title, TeacherId, subjectId } = req.body;
 
     if (req.adminId) {
-      const forum = new Forum({ ...req.body, image: imageName });
+      const forum = new Forum({ ...req.body, SubjectId:subjectId , image: imageName });
       await forum.save();
       res.status(201).json("تم انشاء النادي");
     } else {
