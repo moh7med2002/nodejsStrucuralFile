2b914bca7b7296ec6404b481003ee1cbf653b232
shathakh <shathaqudaih17@gmail.com>
2023-01-29 11:45:53 +0200

    add routes in forum for comments & posts

--- routers/Forum.js
+++ routers/Forum.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const forumController = require("../controllers/Forum");
+const commentController = require("../controllers/comment");
+const postController = require("../controllers/post");
 
 const adminAuth = require("../middelware/AdminAuth");
 const studentAuth = require("../middelware/StudentAuth");
 const teacherAuth = require("../middelware/TeacherAuth");
 
 router.post("/create", adminAuth, forumController.createForum);
+router.get('/posts/:forumId', postController.getAllPost);
+router.get('/comments/:postId',commentController.getAllComment);
 
 module.exports = router;
