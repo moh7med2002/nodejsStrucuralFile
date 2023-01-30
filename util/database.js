7b12ed7673c78f59e782fe2e516f0ebedb9e937f
moh7amed2002 <moha7medheshamabohani@gmail.com>
2023-01-30 10:50:54 +0200

    edit database connection

--- util/database.js
+++ util/database.js
@@ -7,11 +7,11 @@ const DEV_DATABASE_URL='postgres://school_shatha:123456@localhost:5432/school_db
 // local db
 // const sequelize = new Sequelize('school_db','root','059283805928388',{dialect:'mysql',host:"localhost"});
 // const sequelize = new Sequelize(localDbUrl,{dialect:'postgres',host:"localhost"});
-const sequelize = new Sequelize(DEV_DATABASE_URL,{dialect:'postgres',host:"localhost"});
+// const sequelize = new Sequelize(DEV_DATABASE_URL,{dialect:'postgres',host:"localhost"});
 
 
 //  academic db
-// const sequelize = new Sequelize('academ58_schools_db','academ58_root','059283805928388',{dialect:'mysql',host:"localhost"});
+const sequelize = new Sequelize('academ58_schools_db','academ58_root','059283805928388',{dialect:'mysql',host:"localhost"});
 
 //  paletine db
 // const sequelize = new Sequelize('rescteeh_schools_db','rescteeh_root_user','059283805928388',{dialect:'mysql',host:"localhost"});
