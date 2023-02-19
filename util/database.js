352a308109ef5c1464df68fa039075ba4bf71cba
moh7amed2002 <moha7medheshamabohani@gmail.com>
2023-02-19 13:11:37 +0200

    edit database name

--- util/database.js
+++ util/database.js
@@ -14,7 +14,7 @@ const DEV_DATABASE_URL='postgres://school_shatha:123456@localhost:5432/school_db
 const sequelize = new Sequelize('academ58_schools_db','academ58_root','059283805928388',{dialect:'mysql',host:"localhost"});
 
 //  paletine db
-// const sequelize = new Sequelize('rescteeh_schools_db','rescteeh_root_user','059283805928388',{dialect:'mysql',host:"localhost"});
+// const sequelize = new Sequelize('rescteeh_school_db','rescteeh_root_user','059283805928388',{dialect:'mysql',host:"localhost"});
 
 
 module.exports = sequelize;
